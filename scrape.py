c1821d057860fdf0d1ce009167b3030aaa3e098c
Imaad Uddin <imaaduddin@icloud.com>
2021-05-18 15:05:47 -0500

    scraping using beautiful soup

--- scrape.py
+++ scrape.py
@@ -3,19 +3,20 @@ from bs4 import BeautifulSoup
 
 res = requests.get("https://news.ycombinator.com/")
 soup = BeautifulSoup(res.text, "html.parser")
-# links = soup.select(".storylink")
-# votes = soup.select(".score")
-
-# print(votes[0].get("id"))
+links = soup.select(".storylink")
+subtext = soup.select(".subtext")
 
 
-# print(soup.select(".score"))
-
-def create_custom_hn(links, votes):
+def create_custom_hn(links, subtext):
   hn = []
   for idx, item in enumerate(links):
-    title = links[idx].getText()
-    href = links[idx].get("href", None)
-    hn.append(title)
+    title = item.getText()
+    href = item.get("href", None)
+    vote = subtext[idx].select(".score")
+    if len(vote):
+      points = int(vote[0].getText().replace("points", " "))
+      hn.append({"title": title, "link": href, "votes": points})
 
   return hn
+
+create_custom_hn(links, subtext)
