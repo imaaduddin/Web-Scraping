eeba1748cfd3c6ec2564b5cce6a1cefce6c5807e
Imaad Uddin <imaaduddin@icloud.com>
2021-05-20 22:51:23 -0500

    scraping through 2 pages

--- scrape.py
+++ scrape.py
@@ -3,9 +3,22 @@ from bs4 import BeautifulSoup
 import pprint
 
 res = requests.get("https://news.ycombinator.com/")
+res2 = requests.get("https://news.ycombinator.com/news?p=2")
+
 soup = BeautifulSoup(res.text, "html.parser")
+soup2 = BeautifulSoup(res2.text, "html.parser")
+
 links = soup.select(".storylink")
 subtext = soup.select(".subtext")
+
+links2 = soup2.select(".storylink")
+subtext2 = soup2.select(".subtext")
+
+mega_links = links + links2
+mega_subtext = subtext + subtext2
+
+def sort_stories_by_votes(hnlist, key = lambda k:k["votes"], reverse = True):
+  return sorted(hnlist)
 
 
 def create_custom_hn(links, subtext):
@@ -19,6 +32,6 @@ def create_custom_hn(links, subtext):
       if points > 99:
         hn.append({"title": title, "link": href, "votes": points})
 
-  return hn
+  return sort_stories_by_votes(hn)
 
-pprint.pprint(create_custom_hn(links, subtext))
+pprint.pprint(create_custom_hn(mega_links, mega_subtext))
